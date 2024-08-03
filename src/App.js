452e92c3832e318e86932871283e8f09c10c2775
Harish1-1 <109864814+Harish1-1@users.noreply.github.com>
2024-08-03 19:08:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Router>
             <div>
                 <Routes>
+                    <Route path="/" element={<Login />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/dashboard" element={<Dashboard/>}/>
